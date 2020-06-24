55632c3f5e3b870a9a0cd02d3fb349a7b702fe6a
gangduck <GangDuck12@gmail.com>
2020-06-24 19:15:23 +0900

    scrollY

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React,{ useState } from 'react';
 import './style.css';
 
 /**
@@ -7,15 +7,28 @@ import './style.css';
 **/
 
 const Header = (props) => {
+
+  const[toggleOn,setToggle] = useState(false);
+
+  const toggle = () => {
+    setToggle(!toggleOn);
+  }
+
+  const toggleClass = toggleOn? 'toggle active' : 'toggle';
+  const navClass = toggleOn? 'nav active' : 'nav';
+
   return(
     <div className="header">
         <a href="#" className="logo">Home</a>
-        <ul>
+        <ul className={navClass}>
           <li><a href="#project">project</a></li>
           <li><a href="#skils">skils</a></li>
           <li><a href="#interest">interest</a></li>
           <li><a href="#contact">contact</a></li>
         </ul>
+        <div className="toggleIn">
+          <div className={toggleClass} onClick={toggle}></div>
+        </div>
     </div>
    )
 
--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React,{ useEffect } from 'react';
 import tdl from '../../img/tdl.png';
 import cafeweb from '../../img/cafeweb.png';
 import login from '../../img/login.png';
@@ -10,18 +10,52 @@ import './style.css';
 **/
 
 const Porject = (props) => {
+
+  let lastScrollY = 0;
+  let ticking = false;
+
+  useEffect(() =>{
+      window.addEventListener('scroll', handleScroll,true);
+      console.log('compoDidMount');
+      return function(){
+        window.removeEventListener('scroll', handleScroll);
+        console.log('compoWillunmount');
+      } 
+    
+  },[]);
+
+  let nav = React.createRef();
+
+  const handleScroll = () => {
+    lastScrollY = window.scrollY;
+
+    if(!ticking){
+      window.requestAnimationFrame(() => {
+        nav.current.style.top = `${lastScrollY}px`;
+        ticking = false;
+        console.log("lastScrollY",lastScrollY);
+      });
+      ticking = true;
+    }
+  };
+
+  const img1 = 'img1';
+
   return(
     <div className="project" id="project">
+      <div>
+        <nav ref={nav}>avc</nav>
+      </div>
       <h1>Porject</h1>
         <h2>login page</h2>
           <section>
-            <img src={login} style={{width:'350px', height:'200px'}}></img>
+            <img className={img1} src={login} style={{width:'350px', height:'200px'}}></img>
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet iusto, earum, voluptates aspernatur sed est. Ad exercitationem, debitis nisi nostrum repellat atque mollitia molestias sunt velit hic reprehenderit facilis laborum nemo, provident consequuntur libero veniam! Mollitia illum magnam, dignissimos voluptas neque.</p>
           </section>
         <h2>To do list</h2>   
           <section>
-            <img src={tdl} style={{width:'350px', height:'200px'}}></img>
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet iusto, earum, voluptates aspernatur sed est. Ad exercitationem, debitis nisi nostrum repellat atque mollitia molestias sunt velit hic reprehenderit facilis laborum nemo, provident consequuntur libero veniam! Mollitia illum magnam, dignissimos voluptas neque.</p>
+            <img src={tdl} style={{width:'350px', height:'200px'}}></img>
           </section>
         <h2>cafe Web</h2>
           <section>
