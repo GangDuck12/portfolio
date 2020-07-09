e3ed9f2ea195fdf1cec8787cb49d1b7169755df8
gangduck <GangDuck12@gmail.com>
2020-07-09 22:00:16 +0900

    Updates

---
+++ precache-manifest.ca839ac5796c3f7038c6d45c52fe5759.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41e154fa039d2d1f9cafe5273687c553",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "7474e49612bf755a1814",
+    "url": "/portfolio/static/css/main.aceb1d4f.chunk.css"
+  },
+  {
+    "revision": "4d88c9069a9f21a8b902",
+    "url": "/portfolio/static/js/2.3ea24463.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.3ea24463.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7474e49612bf755a1814",
+    "url": "/portfolio/static/js/main.74901e0d.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "343d9dd4f83bb17114d361c72a8bb7d3",
+    "url": "/portfolio/static/media/about-background-img.343d9dd4.jpg"
+  },
+  {
+    "revision": "dcbd910c7611750722b7b36acd261971",
+    "url": "/portfolio/static/media/cafeweb.dcbd910c.png"
+  },
+  {
+    "revision": "df7d52f9c6394c9853f280661cdf495d",
+    "url": "/portfolio/static/media/contact-background-img.df7d52f9.jpg"
+  },
+  {
+    "revision": "71eeddfb87cfea58dede07e2454a31ec",
+    "url": "/portfolio/static/media/img.71eeddfb.jpg"
+  },
+  {
+    "revision": "331dc960589497a94191325f23558395",
+    "url": "/portfolio/static/media/login.331dc960.png"
+  },
+  {
+    "revision": "8b9f30bb19baa48dcf325c15ebeb25c6",
+    "url": "/portfolio/static/media/project-background-img.8b9f30bb.jpg"
+  },
+  {
+    "revision": "229dca7813533ff5241196395ad184d0",
+    "url": "/portfolio/static/media/skills-background-img.229dca78.png"
+  },
+  {
+    "revision": "cb296102c23100dc6cf5dac4f63a5d50",
+    "url": "/portfolio/static/media/tdl.cb296102.png"
+  }
+]);
